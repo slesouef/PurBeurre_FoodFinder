d26a66b44c127837f4a8ae07655b76f1fa728c99
SebastienLesouef <sebastienlesouef@gmail.com>
2018-08-14 12:30:07 +0200

    adding code in main file to connect and check db

--- run.py
+++ run.py
@@ -1,14 +1,25 @@
 #! usr/env/bin python3
 # -*- coding:utf-8 -*-
 """main application file. Contains main loop."""
-from myrequest import *
+# from myrequest import *
+from myconnector import *
 
 
 def main_loop():
-    call = Call()
-    url = call.create_url()
-    call.api_request(url)
-    print(call.data)
+
+    # connect to database
+    db = Database()
+    connection = db.connect()
+
+    # check database content
+    check = db.check_database(connection)
+    print (check)
+
+    #call API to retrieve data
+    # call = Call()
+    # url = call.create_url()
+    # call.api_request(url)
+
     # print("1 - Quel aliment souhaitez-vous remplacer ?" + '\n' + "2 - "
     #                  "Retrouver mes aliments substitués.")
     # choice = input("Votre choix:")
