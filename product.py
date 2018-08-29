187774b58b9d699d117640fff96466e0f24c4525
SebastienLesouef <sebastienlesouef@gmail.com>
2018-08-29 14:35:04 +0200

    added product file to create db data object for connector refactoring

---
+++ product.py
@@ -0,0 +1,30 @@
+#! usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""Manipulate product objects"""
+
+
+class Product:
+
+    def __init__(self, name, quantity, id=None):
+        # data format
+        self.db_format = {"name": "name", "quantity": "quantity", "id": "id"}
+        # initialise data
+        self.name = name
+        self.quantity = quantity
+        self.id = id
+        # initialise table
+        self.table = "test"
+
+    def create_object(self):
+        values = self.db_format
+        values["name"] = self.name
+        values["quantity"] = self.quantity
+        values["id"] = self.id
+        table_name = self.table
+        return values, table_name
+
+    def update_object(self, object, id):
+        data = list(object)
+        value = data[0]
+        value["id"] = id
+        return object
