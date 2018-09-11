332ee24de6bb3afabf8feba87c35c09cebca10d5
SebastienLesouef <sebastienlesouef@gmail.com>
2018-09-11 22:36:16 +0200

    simplified launch checks

--- run.py
+++ run.py
@@ -2,46 +2,41 @@
 # -*- coding:utf-8 -*-
 """run the application"""
 from launch import *
+from product import *
 
 
 def main():
 
     launch = Launch()
-    launch.check_db()
-    if launch.DB_ok is True:
-        launch.check_tables()
-        if launch.tables_ok is True:
-            launch.check_content()
-            if launch.content_ok is True:
-                print(
-                    "1 - Quel aliment souhaitez-vous remplacer ?" + '\n' +
-                    "2 - Retrouver mes aliments substitués."
-                    )
-                choice = input("Votre choix:")
-                if choice == '1':
-                    print("categorie list")
-                elif choice == '2':
-                    print("saved list")
-                else:
-                    print("merci de bien vouloir fournir une valeur valide")
-            else:
-                print("no content")
+    if launch.check_db() and launch.check_tables() and launch.check_content():
+        print(
+            "1 - Quel aliment souhaitez-vous remplacer ?" + '\n' +
+            "2 - Retrouver mes aliments substitués."
+            )
+        choice = input("Votre choix:")
+        if choice == '1':
+            print("categorie list")
+        elif choice == '2':
+            print("saved list")
         else:
-            print("no tables")
+            print("merci de bien vouloir fournir une valeur valide")
     else:
-        print("The database does not exist. Please check the configuration.")
+        print("La base de données est mal configurée. Merci de verifier votre "
+              "configuration.")
 
     # DB CRUD
+    # db = Database()
     # table = Table(db)
     # product = Product("'evian'", "'150ml'", "'evian'", "'eau minerale'",
     #                   "'http://test.off.org'", "'a'", 1, 4)
     # entry = product.create()
+    # print(entry)
     # pid = table.insert(entry)
     # entry = product.update(entry, pid)
     # update = table.update(entry, id=4)
     # read = table.read(entry, id=3)
     # delete = table.delete(entry, id=4)
-    # print(update)
+    # print(pid)
 
     # category = Category("'test3'", None)
     # entry = category.create()
