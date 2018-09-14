6f77a7f08e1d0baf849a773faaf2250448f02325
SebastienLesouef <sebastienlesouef@gmail.com>
2018-09-14 13:49:51 +0200

    method show_products complete

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from myconnector import *
 from categories import *
 from product import *
 
+
 class Controller:
     """receives inputs and updates application screen"""
 
@@ -39,7 +40,7 @@ class Controller:
         # select all categories from database
         category = Category()
         category = category.create()
-        read = table.read(category)
+        read = self.table.read(category)
         # create list of categories
         row = 1
         lookup = {}
@@ -57,6 +58,7 @@ class Controller:
         choice = input("Votre choix:")
         # act on input
         if choice in lookup:
+            cid = None
             for x in read:
                 if x["name"] == lookup[choice]:
                     cid = x["cid"]
@@ -68,15 +70,48 @@ class Controller:
         # display error
         else:
             print("merci de bien vouloir fournir une valeur valide")
+            self.show_categories()
 
     def show_products(self, cid):
         """displays a list of all the products within a categorie.
            one product can be selected."""
-        print("products!!!")
+        # select all product for the category selected
+        product = Product()
+        product = product.create()
+        read = self.table.read(product, cid=cid)
+        # read all database response
+        row = 1
+        lookup = {}
+        for i in read:
+            # create display string
+            name = i["name"]
+            quantity = i["quantity"]
+            prod = str(row) + " - " + name + ", " + quantity
+            # display category
+            print(prod)
+            # update lookup table
+            lookup[str(row)] = i["pid"]
+            # update row id
+            row += 1
+            # wait for input
+        choice = input("Votre choix:")
+        # identify product from input
+        if choice in lookup:
+            pid = [x["pid"] for x in read if x["pid"] == lookup[choice]]
+            pid = pid[0]
+            self.show_substitution(pid)
+        # close app
+        elif choice == "exit":
+            self.db.connection.close()
+            self.close = 1
+        # display error
+        else:
+            print("merci de bien vouloir fournir une valeur valide")
+            self.show_products(cid)
 
-    def show_substitution(self):
+    def show_substitution(self, pid):
         """displays the selected product and a substitution (lower rating)"""
-        pass
+        print(pid)
 
     def save_substition(self):
         """saves the selected product and its substitution in database"""
