b310d51e406879f215d03b53fd79a4abd1d2f8b2
SebastienLesouef <sebastienlesouef@gmail.com>
2018-09-14 18:20:36 +0200

    show_substitution method implemented

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ class Controller:
         if choice in lookup:
             pid = [x["pid"] for x in read if x["pid"] == lookup[choice]]
             pid = pid[0]
-            self.show_substitution(pid)
+            self.show_substitution(cid, pid)
         # close app
         elif choice == "exit":
             self.db.connection.close()
@@ -107,11 +107,63 @@ class Controller:
             print("merci de bien vouloir fournir une valeur valide")
             self.show_products(cid)
 
-    def show_substitution(self, pid):
+    def show_substitution(self, cid, pid):
         """displays the selected product and a substitution (lower rating)"""
-        print(pid)
+        # retrieve product information
+        product = Product()
+        product = product.create()
+        read = self.table.read(product, pid=pid)
+        # read database response
+        name = read[0]["name"]
+        brand = read[0]["brand"]
+        quantity = read[0]["quantity"]
+        description = read[0]["description"]
+        url = read[0]["url"]
+        rating = read[0]["rating"]
+        prod = "nom du produit: " + name + "\n" +\
+               "marque du produit: " + brand + "\n" +\
+               "quantite du produit: " + quantity + "\n" +\
+               "description: " + description + "\n" +\
+               "url: " + url + "\n" +\
+               "rating: " + rating
+        print(prod)
+        read = self.table.read(product, cid=cid)
+        rating_list = {x["pid"]: x["rating"] for x in read}
+        sub_pid = sorted(rating_list, key=rating_list.get)
+        sub_pid = sub_pid[0]
+        read = self.table.read(product, pid=sub_pid)
+        # read database response
+        sub_name = read[0]["name"]
+        sub_brand = read[0]["brand"]
+        sub_quantity = read[0]["quantity"]
+        sub_description = read[0]["description"]
+        sub_url = read[0]["url"]
+        sub_rating = read[0]["rating"]
+        sub = "nom du produit: " + sub_name + "\n" + \
+               "marque du produit: " + sub_brand + "\n" + \
+               "quantite du produit: " + sub_quantity + "\n" + \
+               "description: " + sub_description + "\n" + \
+               "url: " + sub_url + "\n" + \
+               "rating: " + sub_rating
+        print(sub)
+        # propose to save this search
+        choice = input("voulez vous sauvegarder cette recherche (Oui/Non)?")
+        # user wants to save this search
+        if choice.lower() == "oui" or choice.lower() == "o":
+            self.save_substitution(pid, sub_pid)
+        # user does not save this search
+        elif choice.lower() == "non" or choice.lower() == "n":
+            self.landing()
+        # close app
+        elif choice == "exit":
+            self.db.connection.close()
+            self.close = 1
+        # display error
+        else:
+            print("merci de bien vouloir fournir une valeur valide")
+            self.show_substitution(cid, pid)
 
-    def save_substition(self):
+    def save_substitution(self, pid, sub_pid):
         """saves the selected product and its substitution in database"""
         pass
 
